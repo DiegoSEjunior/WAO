c06819d668d0ed3b2d42c3720c45035e7adcb574
Diego Rivaldo <diegosejunior@gmail.com>
2026-09-24 07:17:01 -0500

    Fix GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <CartProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/WAO">
           <App />
         </BrowserRouter>
       </CartProvider>
